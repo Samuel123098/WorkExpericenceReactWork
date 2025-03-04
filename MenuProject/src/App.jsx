e852c3313ce02b5e57b4c91df01b90e4255b001f
Samuel123098 <S4muel2018@hotmail.com>
2025-03-04 11:55:31 +0000

    initialize project structure with React and Vite setup, add basic components and styles

---
+++ MenuProject/src/App.jsx
@@ -0,0 +1,15 @@
+import "./CSS/App.css"
+
+function App() {
+  return (
+    <div>
+        <div className="parent">
+          <div className="SecondaryBackGround">
+
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default App
---
+++ MenuProject/src/Pages/OrderingMenu/main.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <>
+      <h>Get good!</h>
+      </>
+    )
+  }
+  
+  export default App
+  
